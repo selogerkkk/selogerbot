3c430cbe971d1555184e4a0661d660fc8cde6524
selogerkkk <ruanhborges@gmail.com>
2023-05-26 13:02:47 -0300

    implemented regex for pattern search :)

--- telegrambot.py
+++ telegrambot.py
@@ -2,11 +2,14 @@ import os
 import requests
 import time
 from dotenv import load_dotenv
+import re
+import datetime
 
 load_dotenv()
 
 API_TOKEN = os.getenv('API_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
+
 # Função para obter as atualizações do Telegram
 
 
@@ -30,20 +33,14 @@ def send_message(text):
             f"Erro ao enviar a mensagem: {response.status_code} - {response.text}")
         return None
 
-# Função para executar a ação desejada
 
-
+# Função para enviar mensagem
 def perform_action():
-    # Coloque sua ação aqui
-    # Exemplo: enviar uma mensagem para o grupo
     send_message("Mensagem lida!")
 
-# Função para controlar a ação
 
-# Função para processar a mensagem recebida
-
-
-def process_message(message):
+# Função para processar a mensagem recebida sem filtro
+def process_message_without_filter(message):
     if 'text' in message:
         # Extrai o nome do remetente
         sender_name = message['from']['first_name']
@@ -53,8 +50,104 @@ def process_message(message):
         print(f"{sender_name}: {text}")
     else:
         print("Mensagem enviada não é um texto!")
-# Função para controlar a ação
 
+
+# Função para processar a mensagem recebida TRADE
+def process_message(message):
+    # Verifica se a mensagem contém texto
+    if 'text' in message:
+        # Extrai o nome do remetente
+        sender_name = message['from']['first_name']
+        # Extrai o conteúdo da mensagem
+        text = message['text']
+        
+        # Verifica se a mensagem contém o padrão específico
+        if "✅🔥 TRADERZISMO FREE 🔥✅" in text:
+            # Procura um padrão específico na mensagem usando expressões regulares
+            pattern = r"📊 ([^\n]+)\n(🔴 PUT|🟢 CALL)?\n⚠️ Operar (AGORA|\d{2}:\d{2}|\d{2}:\d{2}) ⚠️"
+            match = re.search(pattern, text)
+            
+            # Verifica se o padrão foi encontrado
+            if match:
+                # Extrai as informações desejadas
+                par = match.group(1).strip()
+                direcao = match.group(2).strip() if match.group(2) else "N/A"
+                horario = match.group(3).strip()
+                
+                # Se o horário for "AGORA", substitui pelo horário da mensagem
+                if horario == "AGORA":
+                    horario = "horário da mensagem"
+                
+                # Imprime as informações com o nome do remetente
+                print(f"Mensagem recebida de {sender_name}:")
+                print(f"Par: {par}")
+                print(f"Direção: {direcao}")
+                print(f"Horário: {horario}")
+            else:
+                print("Padrão não encontrado na mensagem.")
+        else:
+            print("Mensagem não contém o padrão esperado.")
+
+    # Verifica se a mensagem contém texto
+    if 'text' in message:
+        # Extrai o nome do remetente
+        sender_name = message['from']['first_name']
+        # Extrai o conteúdo da mensagem
+        text = message['text']
+        
+        # Verifica se a mensagem contém o padrão específico
+        if "✅🔥 TRADERZISMO FREE 🔥✅" in text:
+            # Procura um padrão específico na mensagem usando expressões regulares
+            pattern = r"📊 ([^\n]+)\n(🔴 PUT|🟢 CALL)?\n⚠️ Operar (AGORA|\d{2}:\d{2})"
+            match = re.search(pattern, text)
+            
+            # Verifica se o padrão foi encontrado
+            if match:
+                # Extrai as informações desejadas
+                par = match.group(1).strip()
+                direcao = match.group(2).strip() if match.group(2) else "N/A"
+                horario = match.group(3).strip() if match.group(3) != "AGORA" else "horário da mensagem"
+                
+                # Imprime as informações com o nome do remetente
+                print(f"Mensagem recebida de {sender_name}:")
+                print(f"Par: {par}")
+                print(f"Direção: {direcao}")
+                print(f"Horário: {horario}")
+            else:
+                print("Padrão não encontrado na mensagem.")
+        else:
+            print("Mensagem não contém o padrão esperado.")
+    # Verifica se a mensagem contém texto
+    if 'text' in message:
+        # Extrai o nome do remetente
+        sender_name = message['from']['first_name']
+        # Extrai o conteúdo da mensagem
+        text = message['text']
+        
+        # Verifica se a mensagem contém o padrão específico
+        if " ✅🔥 TRADERZISMO FREE 🔥✅" in text:
+            # Procura um padrão específico na mensagem usando expressões regulares
+            pattern = r"📊 ([^\n]+)\n(🔴PUT|🟢 CALL)?\n⚠️ Operar (AGORA|\das (\d{2}:\d{2}))"
+            match = re.search(pattern, text)
+            
+            # Verifica se o padrão foi encontrado
+            if match:
+                # Extrai as informações desejadas
+                par = match.group(1).strip()
+                direcao = match.group(2).strip() if match.group(2) else "N/A"
+                horario = match.group(3).strip() if match.group(3) != "AGORA" else "horário da mensagem"
+                
+                # Imprime as informações com o nome do remetente
+                print(f"Mensagem recebida de {sender_name}:")
+                print(f"Par: {par}")
+                print(f"Direção: {direcao}")
+                print(f"Horário: {horario}")
+            else:
+                print("Padrão não encontrado na mensagem.")
+        else:
+            print("Mensagem não contém o padrão esperado.")
+
+# Função para não repetir a mensagem
 
 def control_action():
     action_counter = 0
@@ -69,9 +162,8 @@ def control_action():
                     action_counter = last_message_id
                     process_message(last_message)
                     perform_action()
-
         time.sleep(1)
 
 
-# Iniciar o controle da ação
+# Iniciar
 control_action()
