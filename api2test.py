9b8f30ed406f2129a8fc59465860991afd581b37
selogerkkk <ruanhborges@gmail.com>
2023-07-11 11:38:57 -0300

    .

--- api2test.py
+++ api2test.py
@@ -3,18 +3,18 @@ from telethon import events
 
 API_ID = '12093312'
 API_HASH = '67926450017650430bfc865ad771523d'
-# CHAT_ID = '-1001474420372'
-CHAT_ID = '-1001864670859'
+CHAT_ID = '-1001474420372'  # traderzismo
+# CHAT_ID = '-1001864670859'
 
 with TelegramClient('session_name', API_ID, API_HASH) as client:
     @client.on(events.NewMessage)
     async def armazenar_mensagem(event):
         if event.is_group and str(event.chat_id) == CHAT_ID:
-            print(f"Nova mensagem recebida: {event.message.text}")
-        # else:
-        #     print(event.chat_id)
+            mensagem = print(f"Nova mensagem recebida: {event.message.text}")
+
     while True:
         try:
+            print("Conectado ao telegram.")
             client.run_until_disconnected()
         except KeyboardInterrupt:
             loop = False
